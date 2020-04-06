39ced236ef82c39a71639f3c5eaa7b799c848ef0
JimmyVazz <cjvasquez_98@hotmail.com>
2020-04-06 16:22:29 -0500

    primeras practicas

---
+++ practica1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pylab as plt
+ 
+x = np.linspace(-1, 1, 50)
+y1 = 2*x
+y2 = 2*(x)**2 + 1
+y3 = 2*(x)**3 - 2
+ 
+plt.figure(num='MiPrimerGrafico')
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+plt.show()
